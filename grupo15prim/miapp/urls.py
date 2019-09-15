561c98a7d5e2a4640f6529718c3c6efed858fdd9
fernanda <fda.tagle@gmail.com>
2019-09-15 17:03:36 -0300

    agrega reserva de mesas

--- grupo15prim/miapp/urls.py
+++ grupo15prim/miapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('reserva/', reserva, name='reserva'),
+    path('envio/', envioexitoso, name='envioexitoso'),
 ]
